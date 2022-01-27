8f5a4cdaeeb7a859bf6c3eb7ae8b2a632136c37a
pch1656 <pch1656@gmail.com>
2022-01-27 13:57:11 +0900

    commit backend setting

---
+++ backend/src/main/java/com/ssafy/arttab/ArttabApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.arttab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArttabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArttabApplication.class, args);
+    }
+
+}
