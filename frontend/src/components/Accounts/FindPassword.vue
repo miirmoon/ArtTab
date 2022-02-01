fd07d881a07a57cba31cabbab9462967a88b2996
신미래 <sml0206@hanmail.net>
2022-02-01 21:53:31 +0900

    feat: 비밀번호 찾기 페이지 전체 레이아웃 구성

---
+++ frontend/src/components/Accounts/FindPassword.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <!-- 페이지 제목 -->
+    <page-title title="비밀번호 찾기"></page-title>
+
+    <!-- 이메일 입력 -->
+    <p>
+      가입할 때 입력한 이메일 주소를 입력하세요.<br />
+      비밀번호는 초기화되어 메일로 전송됩니다.
+    </p>
+    <input
+      type="email"
+      id="email"
+      class="input-text"
+      name="email"
+      v-model="email"
+      autocapitalize="none"
+    />
+    <span class="alert" v-show="valid.emailType"
+      >이메일 형식이 올바르지 않습니다.</span
+    >
+    <span class="alert" v-show="valid.email">가입되지 않은 이메일입니다.</span>
+
+    <!-- 이메일 재전송 안내 -->
+    <span class="guide"
+      ><alert-circle-outline /> 이메일을 받지 못하셨나요?
+      <span>이메일 재전송하기</span>
+    </span>
+
+    <!-- 이메일 전송 버튼 -->
+    <button
+      :class="{ disabled: !isCompleted }"
+      :disabled="!isCompleted"
+      @click="login"
+    >
+      이메일 전송
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import PageTitle from "@/components/Accounts/child/PageTitle.vue";
+import { AlertCircleOutline } from "mdue";
+
+export default defineComponent({
+  components: {
+    PageTitle,
+    AlertCircleOutline,
+  },
+  data() {
+    return {
+      email: "",
+      valid: {
+        emailType: false,
+        email: false,
+      },
+      isCompleted: false,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/css/reset.scss";
+@import "@/assets/css/common.scss";
+@import "@/assets/css/accounts.scss";
+
+p {
+  line-height: 1.5rem;
+  margin: $size-regular 0;
+}
+</style>
