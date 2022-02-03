41d05a176e5de131fb1877bec1d862871a9f29cd
DECOY <tph01198@naver.com>
2022-02-03 10:12:40 +0900

    feat:Spring 패키지 구조 설정 S06P12B204-241

---
+++ backend/src/main/java/com/ssafy/arttab/artwork/Artwork.java
@@ -0,0 +1,15 @@
+package com.ssafy.arttab.artwork;
+
+/**
+ * @packageName : com.ssafy.arttab.artwork
+ * @fileName : Artwork
+ * @date : 2022-02-03
+ * @language : JAVA
+ * @classification :
+ * @time_limit : 2sec
+ * @required_time : 00:40 ~ 01:22
+ * @submissions : 1
+ * @description :
+ **/
+public class Artwork {
+}
---
+++ backend/src/main/java/com/ssafy/arttab/follow/Follow.java
@@ -0,0 +1,15 @@
+package com.ssafy.arttab.follow;
+
+/**
+ * @packageName : com.ssafy.arttab.follow
+ * @fileName : Follow
+ * @date : 2022-02-03
+ * @language : JAVA
+ * @classification :
+ * @time_limit : 2sec
+ * @required_time : 00:40 ~ 01:22
+ * @submissions : 1
+ * @description :
+ **/
+public class Follow {
+}
