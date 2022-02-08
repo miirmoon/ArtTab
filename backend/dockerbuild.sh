ceeb4e9633019e39bbf3eead99e5026a81781c73
pch1656 <pch1656@gmail.com>
2022-02-08 15:59:00 +0900

    도커파일 수정

--- backend/dockerbuild.sh
+++ backend/dockerbuild.sh
@@ -1,7 +1,7 @@
-docker build -t arttab ./
+sudo docker build -t arttab ./
 
-docker ps -f name=arttab  -q | xargs --no-run-if-empty docker container stop
+sudo docker ps -f name=arttab  -q | xargs --no-run-if-empty docker container stop
 
-docker container ls -a -f name=arttab -q | xargs -r docker container rm
+sudo docker container ls -a -f name=arttab -q | xargs -r docker container rm
 
-docker run -d -p 8080:8080 --name arttab arttab
+sudo docker run -d -p 8080:8080 --name arttab arttab
