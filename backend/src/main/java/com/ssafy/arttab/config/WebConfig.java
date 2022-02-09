d69018ff09dbabc34c7f5a761d1dc3f88fbba6b5
DECOY <tph01198@naver.com>
2022-02-10 04:24:59 +0900

    add WebConfig

---
+++ backend/src/main/java/com/ssafy/arttab/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.ssafy.arttab.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @packageName : com.ssafy.arttab.config
+ * @fileName : WebConfig
+ * @date : 2022-02-10
+ * @language : JAVA
+ * @classification :
+ * @time_limit : 2sec
+ * @required_time : 00:40 ~ 01:22
+ * @submissions : 1
+ * @description :
+ **/
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:80");
+    }
+}
