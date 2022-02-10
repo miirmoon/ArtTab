ea91883dc0044cf4373c4208519349091998f301
haein <haein.hannah.park@gmail.com>
2022-02-10 16:19:26 +0900

    refactor: modal, profile layout, follow-button editted, animate.css added

--- frontend/src/components/profile/ProfileInfo.vue
+++ frontend/src/components/profile/ProfileInfo.vue
@@ -1,37 +1,52 @@
 <template>
-  <div class="black-bg" v-if="isOpen == true">
-    <div class="white-bg">
-      <h4>내 정보 수정 modal이 될 것</h4>
-      <img src="https://via.placeholder.com/150/92c952" />
-      <p>닉네임닉네임닉네임</p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum,
-        maiores tenetur. Incidunt nihil vitae aliquid totam ex maxime sint
-        perferendis.
-      </p>
-      <button @click="changeProfilePic">프로필 사진 변경</button>
-      <button @click="doneEditInfo">완료</button>
-    </div>
-  </div>
+  <!-- Modal -->
+    <transition name="fade" appear>
+      <div class="overlay" v-if="isOpen==true" @click="closeEditModal"></div>
+    </transition>
+    <transition
+      mode="out-in" 
+      enter-active-class="animate__animated animate__fadeInUp"
+      leave-active-class="animate__animated animate__fadeOutDown">
+      <div class="modal" v-if="isOpen==true">
+        <h2>내 정보 수정</h2>
+        <div class="profile-image">
+          <img src="https://via.placeholder.com/150/92c952">
+        </div>
+        <div>
+          <button class="change-profile-pic-btn" @click="changeProfilePic">프로필 사진 변경</button>
+          <label for="nickname" class="label-text">닉네임</label>
+            <input
+              type="nickname"
+              id="nickname"
+              class="input-text"
+              name="nickname"
+            />
+          <label for="bio" class="label-text">소개</label>
+            <input
+              type="bio"
+              id="bio"
+              class="input-text"
+              name="bio"
+            />
+          <button class="done-profile-edit-btn" @click="doneEditInfo">정보 수정 완료</button>
+        </div>
+      </div>
+    </transition>
+
+  <!-- Profile Info -->
   <div>
     <p>Profile Info Component</p>
     <div class="container">
       <div class="profile">
         <div class="profile-image">
-          <img
-            src="https://via.placeholder.com/150/92c952"
-            alt="profile image"
-          />
+          <img src="https://via.placeholder.com/150/92c952" alt="profile image">
         </div>
         <div class="profile-user-settings">
           <h1 class="profile-user-name">닉네임닉네임닉네임</h1>
           <!-- 이메일 조회 빼도 될 듯? -->
           <p class="profile-user-email">email@email.com</p>
-          <button class="btn profile-edit-btn" @click="openEditModal">
-            내 정보 수정
-          </button>
-          <follow-button
-            class="btn profile-edit-btn"
+          <button class="btn profile-edit-btn" @click="openEditModal">내 정보 수정</button>
+          <follow-button class="btn profile-edit-btn"
             :followed="!valid"
             @click="handleFollow"
           ></follow-button>
@@ -44,11 +59,10 @@
           </ul>
         </div>
         <div class="profile-bio">
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum,
-            maiores tenetur. Incidunt nihil vitae aliquid totam ex maxime sint
-            perferendis.
-          </p>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum, maiores tenetur. Incidunt nihil vitae aliquid totam ex maxime sint perferendis.</p>
+        </div>
+        <div class="change-pwd-signout">
+          <p  class="change-pwd-signout-text">비밀번호 변경 및 계정 탈퇴</p>
         </div>
       </div>
     </div>
@@ -74,7 +88,6 @@ export default defineComponent({
       this.valid = !this.valid;
     },
     openEditModal() {
-      console.log("내 정보 수정 modal");
       this.isOpen = true;
     },
     closeEditModal() {
@@ -87,48 +100,103 @@ export default defineComponent({
     changeProfilePic() {
       // 프로필 사진 변경 정보 담아서 BE로 보내는 method
       this.closeEditModal();
-    },
-  },
-});
+    }
+  }
+
+})
 </script>
 
 <style scoped lang="scss">
+// accounts에서 input css 이용
+@import "@/assets/css/accounts.scss";
+
+
 // my profile info edit modal
-.black-bg {
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 98;
   width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5);
-  position: fixed;
+  height:100%;
+  background: rgba(0,0,0,0.6);
   padding: 20px;
 }
-.white-bg {
-  width: 100%;
-  background: white;
-  border-radius: 8px;
+.modal {
+  position: fixed;
+  // modal 위치 가운데로 정렬 필요
+  z-index: 99;
+  // width: 100%;
+  max-width: 40rem;
+  // mobile 크기
+  min-width: 320px;
+  background-color: $white;
+  border-radius: 16px;
   padding: 20px;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.change-profile-pic-btn {
+  font-size: $font-regular;
+  line-height: 1.8;
+  border: 0.1rem solid $black;
+  border-radius: 0.3rem;
+  background: $white;
+  color: $black;
+}
+
+.done-profile-edit-btn {
+  font-size: $font-regular;
+  line-height: 1.8;
+  border: 0.1rem solid $black;
+  border-radius: 0.3rem;
+  background: $black;
+  color: $white;
+}
+
+.change-pwd-signout-text {
+  color: $grey;
+  font-size: $font-small;
+  margin: 0;
+  &:hover {
+    transition: opacity 2s;
+    color: $dark-grey;
+    text-decoration-line: underline;
+  }
 }
 
 // profile
 :root {
-  font-size: 10px;
+    font-size: 10px;
 }
 
 *,
 *::before,
 *::after {
-  box-sizing: border-box;
+    box-sizing: border-box;
 }
 
 body {
-  font-family: "Open Sans", Arial, sans-serif;
-  min-height: 100vh;
-  background-color: #fafafa;
-  color: #262626;
-  padding-bottom: 3rem;
+    font-family: "Open Sans", Arial, sans-serif;
+    min-height: 100vh;
+    background-color: #fafafa;
+    color: #262626;
+    padding-bottom: 3rem;
 }
 
 img {
-  display: block;
+    display: block;
 }
 
 .container {
@@ -140,7 +208,7 @@ img {
 }
 
 .btn {
-  display: inline-block;
+  // display: inline-block;
   font: inherit;
   background: none;
   border: none;
@@ -149,28 +217,24 @@ img {
   cursor: pointer;
 }
 
-.btn:focus {
-  outline: 0.5rem auto #4d90fe;
-}
-
 .visually-hidden {
-  position: absolute !important;
-  height: 1px;
-  width: 1px;
-  overflow: hidden;
-  clip: rect(1px, 1px, 1px, 1px);
+    position: absolute !important;
+    height: 1px;
+    width: 1px;
+    overflow: hidden;
+    clip: rect(1px, 1px, 1px, 1px);
 }
 
 /* Profile Section */
 
 .profile {
-  padding: 5rem 0;
+    padding: 5rem 0;
 }
 
 .profile::after {
-  content: "";
-  display: block;
-  clear: both;
+    content: "";
+    display: block;
+    clear: both;
 }
 
 .profile-image {
@@ -188,33 +252,43 @@ img {
 .profile-user-settings,
 .profile-stats,
 .profile-bio {
-  float: left;
-  width: calc(66.666% - 2rem);
+    float: left;
+    width: calc(66.666% - 2rem);
 }
 
 .profile-user-settings {
   margin-top: 1.1rem;
+  width: auto;
+  // margin: auto;
 }
 
 .profile-user-name {
   display: block;
   // display: block;
   // display: none;
+  float: left;
   font-size: $font-large;
   font-weight: $weight-semi-bold;
+  // user-name 글자가 안무너지게하기위해 임시값(15rem) 지정 username 길이가 달라지면 수치도 변할텐데 이건 모르겠음 ㅠㅠ
+  min-width: 15rem;
 }
 
 .profile-user-email {
-  display: inline-block;
+  // display: inline-block;
+  display: block;
+  color: $dark-grey;
+  margin: auto;
+  font-weight: $weight-light;
 }
 
 .profile-edit-btn {
   font-size: $font-regular;
   line-height: 1.8;
-  border: 0.1rem solid $black;
+  border: 1.5px solid $black;
   border-radius: 0.3rem;
-  padding: 0 2.4rem;
-  margin-left: 2rem;
+  margin: 1rem 0 0.2rem 0;
+  // padding: 0 2.4rem;
+  // margin-left: 2rem;
   // hover시 색상 전환 천천히
   transition: border-color 0.5s, background-color 0.5s, color 0.5s;
   // hover시 색상 전환
@@ -226,35 +300,38 @@ img {
 }
 
 .profile-stats {
-  margin-top: 2.3rem;
+    // margin-top: 2.3rem;
+  margin-top: 1rem;
 }
 
 .profile-stats li {
-  display: inline-block;
-  font-size: 1.6rem;
-  line-height: 1.5;
-  margin-right: 4rem;
-  cursor: pointer;
+    display: inline-block;
+    font-size: 1.6rem;
+    line-height: 1.5;
+    margin-right: 2rem;
+    // margin-right: auto;
+    cursor: pointer;
 }
 
 .profile-stats li:last-of-type {
-  margin-right: 0;
+    margin-right: 0;
 }
 
 .profile-bio {
   font-size: 1.6rem;
   font-weight: 400;
   line-height: 1.5;
-  margin-top: 2.3rem;
+  // margin-top: 2.3rem;
+  margin-top: 1rem;
 }
 
 .profile-stat-count,
 .profile-edit-btn {
-  font-weight: $weight-semi-bold;
+    font-weight: $weight-semi-bold;
 }
 
 /* Media Query */
-// mobile
+// 40rem이하
 
 @media screen and (max-width: 40rem) {
   .profile {
@@ -293,7 +370,7 @@ img {
 
   .profile-user-email {
     display: block;
-    // font-size: $font-large;
+    font-size: $size-medium;
   }
 
   .profile-edit-btn {
@@ -345,62 +422,66 @@ img {
 /* CSS grid */
 
 @supports (display: grid) {
-  .profile {
-    display: grid;
-    grid-template-columns: 1fr 2fr;
-    grid-template-rows: repeat(3, auto);
-    grid-column-gap: 3rem;
-    align-items: center;
-  }
-
-  .profile-image {
-    grid-row: 1 / -1;
-  }
-
-  .gallery {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(22rem, 1fr));
-    grid-gap: 2rem;
-  }
-
-  .profile-image,
-  .profile-user-settings,
-  .profile-stats,
-  .profile-bio,
-  .gallery-item,
-  .gallery {
-    width: auto;
-    margin: 0;
-  }
-
-  @media (max-width: 40rem) {
     .profile {
-      grid-template-columns: auto 1fr;
-      grid-row-gap: 1.5rem;
+      display: grid;
+      grid-template-columns: 1fr 2fr;
+      grid-template-rows: repeat(3, auto);
+      grid-column-gap: 1.5rem;
+      // grid-column-gap: 3rem;
+      align-items: center;
     }
 
     .profile-image {
-      grid-row: 1 / 2;
+      grid-row: 1 / -1;
     }
 
-    .profile-user-settings {
-      display: grid;
-      grid-template-columns: auto 1fr;
-      grid-gap: 1rem;
-    }
-
-    .profile-edit-btn,
+    .profile-image,
+    .profile-user-settings,
     .profile-stats,
     .profile-bio {
-      grid-column: 1 / -1;
+      width: auto;
+      // margin: 0;
+      // margin: auto;
     }
+    .change-pwd-signout {
+      grid-column: -2 / -1;
+      display: flex;
+      justify-content: flex-end;
+    }
+    // CSS grid 40rem 이하 적용
+    @media (max-width: 40rem) {
+      .profile {
+        grid-template-columns: auto 1fr;
+        grid-row-gap: 1.5rem;
+      }
 
-    .profile-user-settings,
-    .profile-edit-btn,
-    .profile-bio,
-    .profile-stats {
-      margin: 0;
+      .profile-image {
+        grid-row: 1 / 2;
+      }
+
+      .profile-user-settings {
+        display: grid;
+        grid-template-columns: auto 1fr;
+        grid-gap: 1rem;
+      }
+
+      .profile-edit-btn,
+      .profile-stats,
+      .profile-bio {
+        grid-column: 1 / -1;
+      }
+
+      .profile-user-settings,
+      .profile-edit-btn,
+      .profile-bio,
+      .profile-stats {
+        margin: 0;
+      }
+
+      .change-pwd-signout {
+        grid-row: 4 / 5;
+      }
     }
-  }
 }
+
 </style>
