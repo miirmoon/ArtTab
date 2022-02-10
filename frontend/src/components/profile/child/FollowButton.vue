efbbc7fef673d6a8787e1e963bac5b3befa29766
haein <haein.hannah.park@gmail.com>
2022-02-10 09:58:08 +0900

    feat: profile page layout, profile info layout added

---
+++ frontend/src/components/profile/child/FollowButton.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <button
+      :class="{'followed': followed}"
+      v-text="buttonText"
+      @click="followUser"
+    ></button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      status: false,
+    };
+  },
+  props: {
+    // user id랑 follows prop 받기
+    followed: {
+      type: Boolean,
+    }
+  },
+  methods: {
+    followUser() {
+      // 여기에 axios 추가
+      this.status = !this.status;
+    }
+  },
+  computed: {
+    buttonText() {
+        return (this.status) ? "팔로우" : "언팔로우";
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+button {
+  font-weight: $weight-semi-bold;
+  &:hover {
+  color: $white;
+  }
+}
+
+</style>
