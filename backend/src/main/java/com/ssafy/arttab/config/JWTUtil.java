39de7965d2ebddc91cdc6399c063eefb7ded6dbe
DECOY <tph01198@naver.com>
2022-02-14 11:07:53 +0900

    feat 로그인기능추가

--- backend/src/main/java/com/ssafy/arttab/config/JWTUtil.java
+++ backend/src/main/java/com/ssafy/arttab/config/JWTUtil.java
@@ -1,5 +1,9 @@
 package com.ssafy.arttab.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
 /**
  * @packageName : com.ssafy.arttab.config
  * @fileName : JWTUtil
@@ -11,9 +15,16 @@ package com.ssafy.arttab.config;
  * @submissions : 1
  * @description :
  **/
+@RequiredArgsConstructor
+@Component
 public class JWTUtil {
-//
-//    @Value("${jwt.algorithm}")
-//    private final String secretKey;
-//    private final validity;
+
+        private String secretKey = "banban";
+
+
+        private long tokenValidTime = 60 * 60 * 1000L;
+        private final UserDetailsService userDetailsService;
+
+
+
 }
