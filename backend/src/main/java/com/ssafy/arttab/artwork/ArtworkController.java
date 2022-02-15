0e58ee52acc8554c28d62c3be469ecec45732338
pch1656 <pch1656@gmail.com>
2022-02-15 09:14:05 +0900

    hotfix: use paths.get

--- backend/src/main/java/com/ssafy/arttab/artwork/ArtworkController.java
+++ backend/src/main/java/com/ssafy/arttab/artwork/ArtworkController.java
@@ -14,6 +14,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -43,7 +45,9 @@ public class ArtworkController {
         LocalDateTime time=LocalDateTime.now();
         String originFileName=file.getOriginalFilename();
         String saveFileName=new MD5Generator(originFileName+time).toString()+file.getOriginalFilename();
-        String upperPath=System.getProperty("user.dir")+ File.separator + "img"; // artwork 디렉토리
+        String upperPath=System.getProperty("user.dir") + "img"+ File.separator; // artwork 디렉토리
+        Path path = Paths.get(".");
+        System.out.println(path.toAbsolutePath());
         String savePath=upperPath; // artwork의 사용자 디렉토리
 
         // 디버깅용
@@ -53,7 +57,8 @@ public class ArtworkController {
         // artwork 디렉토리 없으면 폴더 생성
         if(!new File(upperPath).exists()){
             try{
-                new File(upperPath).mkdir();
+                var status = new File(upperPath).mkdir();
+                System.out.println("디렉토리" + status);
             }
             catch(Exception e){
                 e.getStackTrace();
@@ -64,6 +69,7 @@ public class ArtworkController {
         if(!new File(savePath).exists()){
             try{
                 new File(savePath).mkdir();
+                System.out.println("파일저장폴더 생성");
             }
             catch(Exception e){
                 e.getStackTrace();
