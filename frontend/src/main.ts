85ff59f754dffe4db4b0d64a74ea9bed8adacf5a
backcy855 <backcy855@google.com>
2022-02-16 16:53:32 +0900

    feature/search

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,15 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import MasonryWall from "@yeger/vue-masonry-wall";
-import Vue3DraggableResizable from "vue3-draggable-resizable";
 import VueCookies from "vue3-cookies";
 import "animate.css";
-import "vue3-draggable-resizable/dist/Vue3DraggableResizable.css";
 
 createApp(App)
   .use(store)
   .use(router)
   .use(MasonryWall)
-  .use(Vue3DraggableResizable)
   .use(VueCookies)
   .mount("#app");
