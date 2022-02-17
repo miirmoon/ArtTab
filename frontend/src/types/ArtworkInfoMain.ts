eef7de49faf39821d29cf6252d98f5d6c9b0afd6
haein <haein.hannah.park@gmail.com>
2022-02-17 16:56:55 +0900

    fix: 좋아요 문제 수정

---
+++ frontend/src/types/ArtworkInfoMain.ts
@@ -0,0 +1,11 @@
+export default interface ArtworkInfo {
+  artworkId: number;
+  artworkRegdate: string;
+  artworkTitle: string;
+  imageUrl: string;
+  likeOrNot: boolean;
+  memberId: number;
+  memberNickname: string;
+  saveFileName: string;
+  saveFolder: string;
+}
